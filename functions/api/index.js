c313376145c6fc5b87e17cb714b7e235a5536208
CarValSoft <carvalsoft@gmail.com>
2026-05-07 04:37:54 -0600

    Fetch data from external API and return response

--- functions/api/index.js
+++ functions/api/index.js
@@ -1,15 +1,23 @@
 export async function onRequest() {
 
-  return new Response(
-    JSON.stringify({
-      ok:true,
-      mensaje:"API funcionando"
-    }),
-    {
-      headers:{
-        "Content-Type":"application/json"
-      }
+  const api =
+  "https://script.google.com/macros/s/AKfycbzJH4pDm8u2jOE9-inWxV5-qf0b1_VHylZr1d5X9X5Uo47SDK141umlw8sJMxyA1yDJ/exec?callback=recibirDatos";
+
+  const r = await fetch(api);
+
+  const text = await r.text();
+
+  const clean =
+    text
+    .replace(/^recibirDatos\(/, "")
+    .replace(/\);?$/, "");
+
+  return new Response(clean, {
+
+    headers:{
+      "Content-Type":"application/json"
     }
-  );
+
+  });
 
 }
