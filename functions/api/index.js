4456c3cbcdd6aa2e3062bf81e0b8f08b6eeecd13
CarValSoft <carvalsoft@gmail.com>
2026-05-07 04:58:50 -0600

    Refactor API request handling and response format

--- functions/api/index.js
+++ functions/api/index.js
@@ -1,22 +1,18 @@
 export async function onRequest() {
 
-  const api =
-  "https://script.google.com/macros/s/AKfycbwJGiaovAM65aBjhM6ZRoUZjuGMsJ0JExyMiqjH7pRWbWV6zvT9f59VwirJaPm_Ylp6/exec?callback=recibirDatos";
+  const url =
+  "https://script.google.com/macros/s/AKfycbwJGiaovAM65aBjhM6ZRoUZjuGMsJ0JExyMiqjH7pRWbWV6zvT9f59VwirJaPm_Ylp6/exec";
 
-  const r = await fetch(api);
+  const r = await fetch(url);
 
-  const text = await r.text();
+  const html = await r.text();
 
-  // JSONP -> JSON
-  const clean =
-    text
-    .replace(/^recibirDatos\(/, "")
-    .replace(/\);?$/, "");
+  return new Response(html, {
 
-  return new Response(clean, {
+    headers: {
 
-    headers:{
-      "Content-Type":"application/json"
+      "Content-Type":"text/html;charset=UTF-8"
+
     }
 
   });
